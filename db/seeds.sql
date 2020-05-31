3b27b28c140329320dd7d516c11e443955b0e47f
Jesse Aldana <jesse.aldana@gmail.com>
2020-05-31 10:33:41 -0700

    added database package json and serverjs files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Mushroomburger');
+INSERT INTO burger (name) VALUES ('Carrotburger');
+INSERT INTO burger (name, devoured) VALUES ('Burgerburger', true);
+INSERT INTO burger (name, devoured) VALUES ('PotatoNcheeseburger', true);
+INSERT INTO burger (name, devoured) VALUES ('BurgerburgerDonutburger', true);
+INSERT INTO burger (name) VALUES ('ResponsiblyFarmedSalmonburger');
